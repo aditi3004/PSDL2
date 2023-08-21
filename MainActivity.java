d2ef8213c8a0dff05904637f8246ffbee281b11a
Aditi Sharma <99550333+aditi3004@users.noreply.github.com>
2023-08-21 17:17:40 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.assignment2_661;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Spinner sp1=(Spinner) findViewById(R.id.font_size);
+        TextViewt={}
+
+    }
+}
